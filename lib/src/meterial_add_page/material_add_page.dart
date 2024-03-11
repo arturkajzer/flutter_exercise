7551378f1e0f6db060dfc86a40123af492d8ada9
Iron <soul.collector@o2.pl>
2024-03-11 21:07:05 +0100

    Refactoring

--- lib/src/meterial_add_page/material_add_page.dart
+++ lib/src/meterial_add_page/material_add_page.dart
@@ -66,171 +66,25 @@ class _MaterialAddPageView extends HookWidget {
                         ),
                       ),
                       const SizedBox(height: 20),
-                      DropdownButton<MaterialCategory>(
-                        hint: const Text('Please, select item'),
-                        value: state.data.selectedCategory,
-                        onChanged: materialAddCubit.toggleVisibility,
-                        items: MaterialCategory.values.map((iconLabel) {
-                          return DropdownMenuItem<MaterialCategory>(
-                            value: iconLabel,
-                            child: Row(
-                              children: <Widget>[
-                                Icon(iconLabel.icon),
-                                const SizedBox(width: 10),
-                                Text(iconLabel.label),
-                              ],
-                            ),
-                          );
-                        }).toList(),
-                      ),
+                      _buildCategoryDropdownButton(state, materialAddCubit),
                       const SizedBox(height: 20),
-                      Visibility(
-                        visible: state.data.helmetIsVisible,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text('Name:'),
-                            TextFormField(
-                              controller: helmetNameEditingController,
-                              onChanged: materialAddCubit.updateHelmetName,
-                              validator: (value) {
-                                if (value == null || value.trim().isEmpty) {
-                                  return 'Name is required ';
-                                }
-                                return null;
-                              },
-                            ),
-                            const SizedBox(height: 20),
-                            const Text('Quantity:'),
-                            Row(
-                              children: [
-                                IconButton(
-                                  icon: const Icon(Icons.remove),
-                                  onPressed: materialAddCubit.decrement,
-                                ),
-                                const SizedBox(width: 10),
-                                Text('${state.data.helmetModel.quantity}'),
-                                const SizedBox(width: 10),
-                                IconButton(
-                                  icon: const Icon(Icons.add),
-                                  onPressed: materialAddCubit.increment,
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 20),
-                            Center(
-                              child: MaterialButton(
-                                color: Theme.of(context).colorScheme.primary,
-                                onPressed: () async {
-                                  if (formKey.currentState!.validate()) {
-                                    await materialAddCubit.submitHelmetData(
-                                      HelmetModel(
-                                        name: state.data.helmetModel.name,
-                                        quantity:
-                                            state.data.helmetModel.quantity,
-                                      ),
-                                    );
-                                    context.pop<bool>(true);
-                                  }
-                                },
-                                child: const Text(
-                                  'Add helmet',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
+                      _buildHelmetSection(
+                        materialAddCubit,
+                        state,
+                        context,
+                        formKey,
+                        helmetNameEditingController,
                       ),
-                      Visibility(
-                        visible: state.data.ladderIsVisible,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text('Name:'),
-                            TextFormField(
-                              controller: ladderNameEditingController,
-                              onChanged: materialAddCubit.updateLadderName,
-                              validator: (value) {
-                                if (value == null || value.trim().isEmpty) {
-                                  return 'Name is required ';
-                                }
-                                return null;
-                              },
-                            ),
-                            const SizedBox(height: 20),
-                            const Text('Maximum working height:'),
-                            TextFormField(
-                              controller: ladderMaximumHeightEditingController,
-                              keyboardType: TextInputType.number,
-                              onChanged: (value) {
-                                materialAddCubit.updateLadderMaximumHeight(
-                                    int.parse(value));
-                              },
-                              validator: (value) {
-                                if (value == null || value.trim().isEmpty) {
-                                  return 'Maximum working height is required ';
-                                }
-                                return null;
-                              },
-                            ),
-                            const SizedBox(height: 20),
-                            const Text('Ladder load capacity:'),
-                            TextFormField(
-                              keyboardType: TextInputType.number,
-                              controller: ladderLoadCapacityEditingController,
-                              onChanged: (value) {
-                                materialAddCubit
-                                    .updateLadderLoadCapacity(int.parse(value));
-                              },
-                              validator: (value) {
-                                if (value == null || value.trim().isEmpty) {
-                                  return 'Ladder load capacity is required ';
-                                }
-                                return null;
-                              },
-                            ),
-                            const SizedBox(height: 20),
-                            Center(
-                              child: MaterialButton(
-                                color: Theme.of(context).colorScheme.primary,
-                                onPressed: () async {
-                                  if (formKey.currentState!.validate()) {
-                                    await materialAddCubit.submitLadderData(
-                                      LadderModel(
-                                        name: state.data.ladderModel.name,
-                                        ladderLoadCapacityInKg: state.data
-                                            .ladderModel.ladderLoadCapacityInKg,
-                                        maximumWorkingHeightInCm: state
-                                            .data
-                                            .ladderModel
-                                            .maximumWorkingHeightInCm,
-                                      ),
-                                    );
-                                    context.pop<bool>(true);
-                                  }
-                                },
-                                child: const Text(
-                                  'Add ladder',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
+                      _buildLadderSection(
+                        materialAddCubit,
+                        state,
+                        context,
+                        formKey,
+                        ladderNameEditingController,
+                        ladderLoadCapacityEditingController,
+                        ladderMaximumHeightEditingController,
                       ),
-                      Visibility(
-                        visible: state.data.scaffoldPartIsVisible,
-                        child: const Column(
-                          children: [
-                            Text('SCAFFOLD PART'),
-                          ],
-                        ),
-                      ),
+                      _buildScaffoldPartSection(state),
                       const SizedBox(height: 20),
                     ],
                   ),
@@ -242,4 +96,192 @@ class _MaterialAddPageView extends HookWidget {
       ),
     );
   }
+}
+
+Widget _buildCategoryDropdownButton(
+    MaterialAddState state, MaterialAddCubit materialAddCubit) {
+  return DropdownButton<MaterialCategory>(
+    hint: const Text('Please, select item'),
+    value: state.data.selectedCategory,
+    onChanged: materialAddCubit.toggleVisibility,
+    items: MaterialCategory.values.map((iconLabel) {
+      return DropdownMenuItem<MaterialCategory>(
+        value: iconLabel,
+        child: Row(
+          children: <Widget>[
+            Icon(iconLabel.icon),
+            const SizedBox(width: 10),
+            Text(iconLabel.label),
+          ],
+        ),
+      );
+    }).toList(),
+  );
+}
+
+Widget _buildHelmetSection(
+  MaterialAddCubit materialAddCubit,
+  MaterialAddState state,
+  BuildContext context,
+  GlobalKey<FormState> formKey,
+  TextEditingController helmetNameEditingController,
+) {
+  return Visibility(
+    visible: state.data.helmetIsVisible,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Name:'),
+        TextFormField(
+          controller: helmetNameEditingController,
+          onChanged: materialAddCubit.updateHelmetName,
+          validator: (value) {
+            if (value == null || value.trim().isEmpty) {
+              return 'Name is required ';
+            }
+            return null;
+          },
+        ),
+        const SizedBox(height: 20),
+        const Text('Quantity:'),
+        Row(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.remove),
+              onPressed: materialAddCubit.decrement,
+            ),
+            const SizedBox(width: 10),
+            Text('${state.data.helmetModel.quantity}'),
+            const SizedBox(width: 10),
+            IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: materialAddCubit.increment,
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        Center(
+          child: MaterialButton(
+            color: Theme.of(context).colorScheme.primary,
+            onPressed: () async {
+              if (formKey.currentState!.validate()) {
+                await materialAddCubit.submitHelmetData(
+                  HelmetModel(
+                    name: state.data.helmetModel.name,
+                    quantity: state.data.helmetModel.quantity,
+                  ),
+                );
+                context.pop<bool>(true);
+              }
+            },
+            child: const Text(
+              'Add helmet',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _buildLadderSection(
+  MaterialAddCubit materialAddCubit,
+  MaterialAddState state,
+  BuildContext context,
+  GlobalKey<FormState> formKey,
+  TextEditingController ladderNameEditingController,
+  TextEditingController ladderLoadCapacityEditingController,
+  TextEditingController ladderMaximumHeightEditingController,
+) {
+  return Visibility(
+    visible: state.data.ladderIsVisible,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Name:'),
+        TextFormField(
+          controller: ladderNameEditingController,
+          onChanged: materialAddCubit.updateLadderName,
+          validator: (value) {
+            if (value == null || value.trim().isEmpty) {
+              return 'Name is required ';
+            }
+            return null;
+          },
+        ),
+        const SizedBox(height: 20),
+        const Text('Maximum working height:'),
+        TextFormField(
+          controller: ladderMaximumHeightEditingController,
+          keyboardType: TextInputType.number,
+          onChanged: (value) {
+            materialAddCubit.updateLadderMaximumHeight(int.parse(value));
+          },
+          validator: (value) {
+            if (value == null || value.trim().isEmpty) {
+              return 'Maximum working height is required ';
+            }
+            return null;
+          },
+        ),
+        const SizedBox(height: 20),
+        const Text('Ladder load capacity:'),
+        TextFormField(
+          keyboardType: TextInputType.number,
+          controller: ladderLoadCapacityEditingController,
+          onChanged: (value) {
+            materialAddCubit.updateLadderLoadCapacity(int.parse(value));
+          },
+          validator: (value) {
+            if (value == null || value.trim().isEmpty) {
+              return 'Ladder load capacity is required ';
+            }
+            return null;
+          },
+        ),
+        const SizedBox(height: 20),
+        Center(
+          child: MaterialButton(
+            color: Theme.of(context).colorScheme.primary,
+            onPressed: () async {
+              if (formKey.currentState!.validate()) {
+                await materialAddCubit.submitLadderData(
+                  LadderModel(
+                    name: state.data.ladderModel.name,
+                    ladderLoadCapacityInKg:
+                        state.data.ladderModel.ladderLoadCapacityInKg,
+                    maximumWorkingHeightInCm:
+                        state.data.ladderModel.maximumWorkingHeightInCm,
+                  ),
+                );
+                context.pop<bool>(true);
+              }
+            },
+            child: const Text(
+              'Add ladder',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _buildScaffoldPartSection(
+  MaterialAddState state,
+) {
+  return Visibility(
+    visible: state.data.scaffoldPartIsVisible,
+    child: const Column(
+      children: [
+        Text('SCAFFOLD PART'),
+      ],
+    ),
+  );
 }
